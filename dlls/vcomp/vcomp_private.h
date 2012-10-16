51009d34f5fdca918d182a85015cdb32824b70c7
Dan Kegel <dank@kegel.com>
2012-10-15 21:01:30 -0700

    vcomp: better stubs for _vcomp_for_dynamic_init, _vcomp_for_dynamic_next

--- dlls/vcomp/vcomp_private.h
+++ dlls/vcomp/vcomp_private.h
@@ -1,4 +1,7 @@
 /*
+ * vcmp wine internal private include file
+ *
+ * Copyright 2012 Dan Kegel
  * Copyright 2015 Sebastian Lackner
  *
  * This library is free software; you can redistribute it and/or
@@ -16,4 +19,38 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#ifndef __WINE_VCOMP_PRIVATE_H
+#define __WINE_VCOMP_PRIVATE_H
+
+struct vcomp_team
+{
+    struct vcomp_team *parent;
+    union
+    {
+        struct
+        {
+            int counter;
+            int step;
+            int iterations_remaining;
+            int chunksize;
+            int flags;
+        } dyn_for;
+    } work;
+};
+
 extern DWORD vcomp_context_tls DECLSPEC_HIDDEN;
+
+static inline struct vcomp_team *vcomp_get_team(void)
+{
+    return (struct vcomp_team *)TlsGetValue(vcomp_context_tls);
+}
+
+static inline void vcomp_set_team(struct vcomp_team *team)
+{
+    TlsSetValue(vcomp_context_tls, team);
+}
+
+#define VCOMP_DYNAMIC_FOR_FLAGS_DOWN 0x0
+#define VCOMP_DYNAMIC_FOR_FLAGS_UP 0x40
+
+#endif
