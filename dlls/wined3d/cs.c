854cecc5cd5b3d71b8de49536280682fe1ca7822
Stefan Dösinger <stefan@codeweavers.com>
2013-09-27 16:58:13 +0200

    wined3d: Fence color_fill operations.

--- dlls/wined3d/cs.c
+++ dlls/wined3d/cs.c
@@ -1866,6 +1866,8 @@ static UINT wined3d_cs_exec_clear_rtv(struct wined3d_cs *cs, const void *data)
     else
         op->blitter->depth_fill(device, op->view, &op->rect, op->flags, op->depth, op->stencil);
 
+    wined3d_resource_release(op->view->resource);
+
     return sizeof(*op);
 }
 
@@ -1885,6 +1887,8 @@ void wined3d_cs_emit_clear_rtv(struct wined3d_cs *cs, struct wined3d_rendertarge
     op->depth = depth;
     op->stencil = stencil;
     op->blitter = blitter;
+
+    wined3d_resource_acquire(view->resource);
 
     cs->ops->submit(cs, sizeof(*op));
 }
