9c65b2c16ce21835ada85230db0f2d5fba0ededd
Stefan Dösinger <stefan@codeweavers.com>
2013-10-02 22:54:57 +0200

    wined3d: Clean up volume resource data through the CS.

--- dlls/wined3d/volume.c
+++ dlls/wined3d/volume.c
@@ -311,10 +311,8 @@ void wined3d_volume_destroy(struct wined3d_volume *volume)
     TRACE("volume %p.\n", volume);
 
     resource_cleanup(&volume->resource);
-    if (wined3d_settings.cs_multithreaded)
-        device->cs->ops->finish(device->cs);
     volume->resource.parent_ops->wined3d_object_destroyed(volume->resource.parent);
-    HeapFree(GetProcessHeap(), 0, volume);
+    wined3d_cs_emit_volume_cleanup(device->cs, volume);
 }
 
 static void volume_unload(struct wined3d_resource *resource)
@@ -420,6 +418,11 @@ static ULONG volume_resource_incref(struct wined3d_resource *resource)
     TRACE("Forwarding to container %p.\n", volume->container);
 
     return wined3d_texture_incref(volume->container);
+}
+
+void wined3d_volume_cleanup_cs(struct wined3d_volume *volume)
+{
+    HeapFree(GetProcessHeap(), 0, volume);
 }
 
 static ULONG volume_resource_decref(struct wined3d_resource *resource)
