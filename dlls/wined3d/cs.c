7e32429eb45983a9f7c70bb1c26a31ff89df2afa
Stefan Dösinger <stefan@codeweavers.com>
2013-10-24 22:38:26 +0200

    wined3d: Fence preload operations.
    Otherwise texture data might get lost, thanks to surface/volume_evict_sysmem:
    
    1) preload(texture)
    2) map(texture)
        -> Loads sysmem / sets it with discard map.
    3) unmap(texture)
        -> enqueues a resource_changed operation
    4) preload is executed.
        -> Uploads data written in 3. Discards sysmem. Questionable if
           DISCARD was passed, but without it, no harm done.
        -> texture is only up to date location.
    5) resource_changed executed.
        -> Discards texture.
    
    And data is lost. This patch prevents that by stalling the map until the
    PreLoad is done.
    
    What's left open is finding out why anyone (I'm looking at you EVE
    online) preloads a texture right before mapping it...

--- dlls/wined3d/cs.c
+++ dlls/wined3d/cs.c
@@ -2153,6 +2153,8 @@ static UINT wined3d_cs_exec_texture_preload(struct wined3d_cs *cs, const void *d
     wined3d_texture_load(texture, context, texture->flags & WINED3D_TEXTURE_IS_SRGB);
     context_release(context);
 
+    wined3d_resource_dec_fence(&texture->resource);
+
     return sizeof(*op);
 }
 
@@ -2163,6 +2165,8 @@ void wined3d_cs_emit_texture_preload(struct wined3d_cs *cs, struct wined3d_textu
     op = cs->ops->require_space(cs, sizeof(*op));
     op->opcode = WINED3D_CS_OP_TEXTURE_PRELOAD;
     op->texture = texture;
+
+    wined3d_resource_inc_fence(&texture->resource);
 
     cs->ops->submit(cs, sizeof(*op));
 }
@@ -2369,6 +2373,8 @@ static UINT wined3d_cs_exec_buffer_preload(struct wined3d_cs *cs, const void *da
     buffer_internal_preload(op->buffer, context, NULL);
     context_release(context);
 
+    wined3d_resource_dec_fence(&op->buffer->resource);
+
     return sizeof(*op);
 }
 
@@ -2379,6 +2385,8 @@ void wined3d_cs_emit_buffer_preload(struct wined3d_cs *cs, struct wined3d_buffer
     op = cs->ops->require_space(cs, sizeof(*op));
     op->opcode = WINED3D_CS_OP_BUFFER_PRELOAD;
     op->buffer = buffer;
+
+    wined3d_resource_inc_fence(&buffer->resource);
 
     cs->ops->submit(cs, sizeof(*op));
 }
