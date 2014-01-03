71872a39b1bd3f9692361aa40172e102bc38c955
Stefan Dösinger <stefan@codeweavers.com>
2014-01-04 00:53:47 +0100

    wined3d: Remove surface_validate_location.

--- dlls/wined3d/arb_program_shader.c
+++ dlls/wined3d/arb_program_shader.c
@@ -7918,7 +7918,7 @@ static void arbfp_blit_surface(struct wined3d_device *device, enum wined3d_blit_
 
     context_release(context);
 
-    surface_validate_location(dst_surface, dst_surface->container->resource.draw_binding);
+    wined3d_resource_validate_location(&dst_surface->resource, dst_surface->container->resource.draw_binding);
     surface_invalidate_location(dst_surface, ~dst_surface->container->resource.draw_binding);
 }
 
