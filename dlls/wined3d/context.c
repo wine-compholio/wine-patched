c74b8aca5e942b4403fb7be898a665b2b4af807e
Stefan Dösinger <stefan@codeweavers.com>
2014-01-04 01:02:15 +0100

    wined3d: Remove surface_invalidate_location.

--- dlls/wined3d/context.c
+++ dlls/wined3d/context.c
@@ -3218,7 +3218,7 @@ static void context_setup_target(struct wined3d_context *context, struct wined3d
             if (texture->texture_srgb.name)
                 wined3d_texture_load(texture, context, TRUE);
             wined3d_texture_load(texture, context, FALSE);
-            surface_invalidate_location(context->current_rt, WINED3D_LOCATION_DRAWABLE);
+            wined3d_resource_invalidate_location(&context->current_rt->resource, WINED3D_LOCATION_DRAWABLE);
         }
     }
 
