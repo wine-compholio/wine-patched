d2715df81ad5dd356944aa31c465aa97b35d91fb
Erich E. Hoover <erich.e.hoover@gmail.com>
2014-01-17 12:35:57 -0700

    ws2_32: Check for invalid parameters in TransmitFile.

--- dlls/ws2_32/socket.c
+++ dlls/ws2_32/socket.c
@@ -2530,8 +2530,29 @@ static BOOL WINAPI WS2_AcceptEx(SOCKET listener, SOCKET acceptor, PVOID dest, DW
 static BOOL WINAPI WS2_TransmitFile( SOCKET s, HANDLE h, DWORD total_bytes, DWORD bytes_per_send,
                                      LPOVERLAPPED overlapped, LPTRANSMIT_FILE_BUFFERS buffers, DWORD flags )
 {
+    union generic_unix_sockaddr uaddr;
+    unsigned int uaddrlen = sizeof(uaddr);
+    int fd;
+
     FIXME("(%lx, %p, %d, %d, %p, %p, %d): stub !\n", s, h, total_bytes, bytes_per_send, overlapped, buffers,
                                                      flags );
+
+    fd = get_sock_fd( s, 0, NULL );
+    if (fd == -1)
+    {
+        SetLastError( WSAENOTSOCK );
+        return FALSE;
+    }
+    if (getpeername( fd, &uaddr.addr, &uaddrlen ) != 0)
+    {
+        release_sock_fd( s, fd );
+        WSASetLastError( WSAENOTCONN );
+        return FALSE;
+    }
+    release_sock_fd( s, fd );
+    if (flags)
+        FIXME("Flags are not currently supported (0x%x).\n", flags);
+
     WSASetLastError( WSAEOPNOTSUPP );
     return FALSE;
 }
