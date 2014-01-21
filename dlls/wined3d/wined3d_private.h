6c1c59e6cc769a8bdae1f11d87577c81b31c43fc
Stefan Dösinger <stefan@codeweavers.com>
2014-01-21 12:22:30 +0100

    wined3d: Move surface locations into the resource.

--- dlls/wined3d/wined3d_private.h
+++ dlls/wined3d/wined3d_private.h
@@ -2313,7 +2313,6 @@ struct wined3d_surface
     const struct wined3d_surface_ops *surface_ops;
     struct wined3d_texture *container;
     void *user_memory;
-    DWORD locations;
 
     DWORD flags;
 
