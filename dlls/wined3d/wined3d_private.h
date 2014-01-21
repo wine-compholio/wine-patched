292fdac14d2ea19afb993ab487ed00dac12f9137
Stefan Dösinger <stefan@codeweavers.com>
2014-01-21 13:25:48 +0100

    wined3d: Remove surface->pbo.

--- dlls/wined3d/wined3d_private.h
+++ dlls/wined3d/wined3d_private.h
@@ -2310,8 +2310,6 @@ struct wined3d_surface
     UINT pow2Width;
     UINT pow2Height;
 
-    /* PBO */
-    GLuint                    pbo;
     GLuint rb_multisample;
     GLuint rb_resolved;
     GLenum texture_target;
