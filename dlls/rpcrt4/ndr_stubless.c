132626ab521345a98258e6594a82214e09c0f7d8
Jérôme Gardou <jerome.gardou@reactos.org>
2014-06-16 15:55:01 +0200

    rpcrt4: Free parameters allocated by application before anything else.
    The freer function calculates conformance and variance using values in
    parameters which can be freed earlier in the loop, so it is necessary to
    free those before anything else.
    See http://bugs.winehq.org/show_bug.cgi?id=36743 for details.

--- dlls/rpcrt4/ndr_stubless.c
+++ dlls/rpcrt4/ndr_stubless.c
@@ -1145,6 +1145,18 @@ static LONG_PTR *stub_do_args(MIDL_STUB_MESSAGE *pStubMsg,
     unsigned int i;
     LONG_PTR *retval_ptr = NULL;
 
+    if (phase == STUBLESS_FREE)
+    {
+        for (i = 0; i < number_of_params; i++)
+        {
+            unsigned char *pArg = pStubMsg->StackTop + params[i].stack_offset;
+            if (params[i].attr.MustFree)
+            {
+                call_freer(pStubMsg, pArg, &params[i]);
+            }
+        }
+    }
+
     for (i = 0; i < number_of_params; i++)
     {
         unsigned char *pArg = pStubMsg->StackTop + params[i].stack_offset;
@@ -1162,11 +1174,7 @@ static LONG_PTR *stub_do_args(MIDL_STUB_MESSAGE *pStubMsg,
                 call_marshaller(pStubMsg, pArg, &params[i]);
             break;
         case STUBLESS_FREE:
-            if (params[i].attr.MustFree)
-            {
-                call_freer(pStubMsg, pArg, &params[i]);
-            }
-            else if (params[i].attr.ServerAllocSize)
+            if (params[i].attr.ServerAllocSize)
             {
                 HeapFree(GetProcessHeap(), 0, *(void **)pArg);
             }
