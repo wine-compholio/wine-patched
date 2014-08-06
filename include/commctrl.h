9f1a2789e764f9aaa6e0471e6c3af77ef398d50f
Michael Müller <michael@fds-team.de>
2014-08-07 01:41:25 +0200

    comctl32: Implement LoadIconMetric function.

--- include/commctrl.h
+++ include/commctrl.h
@@ -1373,6 +1373,12 @@ typedef struct tagTBADDBITMAP {
 #define HIST_ADDTOFAVORITES     3
 #define HIST_VIEWTREE           4
 
+enum _LI_METRIC
+{
+   LIM_SMALL,
+   LIM_LARGE,
+};
+
 typedef struct tagTBSAVEPARAMSA {
     HKEY   hkr;
     LPCSTR pszSubKey;
