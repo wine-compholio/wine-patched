e74d4543a039ec7eb28d2fbf3af1e566135b5912
Stefan Dösinger <stefan@codeweavers.com>
2014-08-29 12:12:11 +0200

    Winex11: complain about glfinish

--- dlls/winex11.drv/opengl.c
+++ dlls/winex11.drv/opengl.c
@@ -2004,6 +2004,7 @@ static void wglFinish(void)
     escape.code = X11DRV_FLUSH_GL_DRAWABLE;
     escape.gl_drawable = 0;
 
+    ERR("glFinish\n");
     if ((gl = get_gl_drawable( WindowFromDC( ctx->hdc ), 0 )))
     {
         switch (gl->type)
@@ -2029,6 +2030,7 @@ static void wglFlush(void)
     escape.code = X11DRV_FLUSH_GL_DRAWABLE;
     escape.gl_drawable = 0;
 
+    ERR("glFlush\n");
     if ((gl = get_gl_drawable( WindowFromDC( ctx->hdc ), 0 )))
     {
         switch (gl->type)
