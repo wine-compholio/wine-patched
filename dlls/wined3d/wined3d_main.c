f34611223c7929d4821ed48a432aed147b0b0056
Michael Müller <michael@fds-team.de>
2014-09-20 02:48:07 +0200

    wined3d: Add support for DXTn software decoding through libtxc_dxtn. (rev 3)
    Changes in rev 2:
    * Do not use dxtn library when some imports are missing.
    * Do not advertise dxtn converter functions when they are not supported (because
      of missing library or support not compiled in).
    
    Changes in rev 3:
    * Do not require txc_dxtn at compile time by trying some fallback paths.

--- dlls/wined3d/wined3d_main.c
+++ dlls/wined3d/wined3d_main.c
@@ -319,6 +319,8 @@ static BOOL wined3d_dll_init(HINSTANCE hInstDLL)
     if (appkey) RegCloseKey( appkey );
     if (hkey) RegCloseKey( hkey );
 
+    wined3d_dxtn_init();
+
     return TRUE;
 }
 
@@ -350,6 +352,9 @@ static BOOL wined3d_dll_destroy(HINSTANCE hInstDLL)
 
     DeleteCriticalSection(&wined3d_wndproc_cs);
     DeleteCriticalSection(&wined3d_cs);
+
+    wined3d_dxtn_free();
+
     return TRUE;
 }
 
