02eb6a8aa8b9c893aef42d8fc04372aad3006ee5
Felix Yan <felixonmars@gmail.com>
2014-09-23 23:22:17 +0800

    winex11.drv: Update a candidate window's position with over-the-spot style. (try 2)
    In the current implementation, the candidate window position of a input
    method is fixed because XNSpotLocation isn't updated after an input
    context (XIC) is created in X11DRV_CreateIC().
    X11DRV_UpdateCandidatePos() in this patch updates the position. You can
    see the change of a position with ibus, scim or fcitx when input style
    is set to "over the spot" in the registry key:
    
    [HKEY_CURRENT_USER\Software\Wine\X11 Driver]
    "InputStyle"="OverTheSpot"
    
    This patch was based on the original work by Muneyuki Noguchi, and
    received a lot of help from Sebastian Lackner.

--- dlls/user32/driver.c
+++ dlls/user32/driver.c
@@ -154,6 +154,7 @@ static const USER_DRIVER *load_driver(void)
         GET_USER_FUNC(WindowPosChanging);
         GET_USER_FUNC(WindowPosChanged);
         GET_USER_FUNC(SystemParametersInfo);
+        GET_USER_FUNC(UpdateCandidatePos);
 #undef GET_USER_FUNC
     }
 
@@ -511,6 +512,10 @@ static BOOL CDECL nulldrv_SystemParametersInfo( UINT action, UINT int_param, voi
     return FALSE;
 }
 
+static void CDECL nulldrv_UpdateCandidatePos( HWND hwnd, const RECT *caret_rect )
+{
+}
+
 static USER_DRIVER null_driver =
 {
     /* keyboard functions */
@@ -571,7 +576,9 @@ static USER_DRIVER null_driver =
     nulldrv_WindowPosChanging,
     nulldrv_WindowPosChanged,
     /* system parameters */
-    nulldrv_SystemParametersInfo
+    nulldrv_SystemParametersInfo,
+    /* candidate pos functions */
+    nulldrv_UpdateCandidatePos
 };
 
 
@@ -766,6 +773,12 @@ static BOOL CDECL loaderdrv_UpdateLayeredWindow( HWND hwnd, const UPDATELAYEREDW
     return load_driver()->pUpdateLayeredWindow( hwnd, info, window_rect );
 }
 
+static void CDECL loaderdrv_UpdateCandidatePos( HWND hwnd, const RECT *caret_rect )
+{
+    load_driver()->pUpdateCandidatePos( hwnd, caret_rect );
+}
+
+
 static USER_DRIVER lazy_load_driver =
 {
     /* keyboard functions */
@@ -826,5 +839,7 @@ static USER_DRIVER lazy_load_driver =
     nulldrv_WindowPosChanging,
     nulldrv_WindowPosChanged,
     /* system parameters */
-    nulldrv_SystemParametersInfo
+    nulldrv_SystemParametersInfo,
+    /* candidate pos functions */
+    loaderdrv_UpdateCandidatePos
 };
