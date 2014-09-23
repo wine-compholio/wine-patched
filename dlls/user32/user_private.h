025ad5809f420b5af47b65cc6e0f26b3ca1d85ed
Felix Yan <felixonmars@gmail.com>
2014-09-23 23:22:17 +0800

    winex11.drv: Update a candidate window's position with over-the-spot style. (try 2)
    In the current implementation, the candidate window position of a input
    method is fixed because XNSpotLocation isn't updated after an input
    context (XIC) is created in X11DRV_CreateIC().
    X11DRV_UpdateCandidatePos() in this patch updates the position. You can
    see the change of a position with ibus, scim or fcitx when input style
    is set to "over the spot" in the registry key:
    
    [HKEY_CURRENT_USER\Software\Wine\X11 Driver]
    "InputStyle"="OverTheSpot"
    
    This patch was based on the original work by Muneyuki Noguchi, and
    received a lot of help from Sebastian Lackner.

--- dlls/user32/user_private.h
+++ dlls/user32/user_private.h
@@ -117,6 +117,8 @@ typedef struct tagUSER_DRIVER {
     void   (CDECL *pWindowPosChanged)(HWND,HWND,UINT,const RECT *,const RECT *,const RECT *,const RECT *,struct window_surface*);
     /* system parameters */
     BOOL   (CDECL *pSystemParametersInfo)(UINT,UINT,void*,UINT);
+    /* candidate pos functions */
+    void   (CDECL *pUpdateCandidatePos)(HWND,const RECT *);
 } USER_DRIVER;
 
 extern const USER_DRIVER *USER_Driver DECLSPEC_HIDDEN;
