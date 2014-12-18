5118cd86fb5fe5576122f3bf21ebf72880cc6516
Sebastian Lackner <sebastian@fds-team.de>
2014-12-18 01:04:34 +0100

    dinput: Ensure X11 input events are handled even without explicit message loop. (v4)
    This commit basically reverts b22ff8018aca7c365e505f1db7732f7050ae259b.
    
    Michael Müller did a full analysis of this problem, which reveals that the issue
    is caused by the way user32 and winex11 works. Wine establishes separate X server
    connections for each thread, so each thread needs to call
    USER_Driver->pMsgWaitForMultipleObjectsEx from time to time to ensure that events
    are properly forwarded to the wineserver. On Windows all this isn't necessary
    because the kernel itself is responsible for generating window events, and there
    is no need to pass them from the application to the kernel.
    
    A proper fix would mean to rewrite winex11 to always handle X11 events
    asynchronously, and without explicit need for a message loop. Since such a rewrite
    is rather unlikely and will not happen during the near future, adding a workaround
    till a better solution is found. This workaround will have no disadvantage, except
    that this isn't a complete fix. Please note that not only hooks are affected by
    this issue, also *RawInput* functions show the same issue - which is the reason why
    native dinput will still not work (already tested).
    
    Since games use very different code in order to query for input events, we have to
    call __wine_check_for_events from multiple locations in the code. See:
    * https://bugs.wine-staging.com/show_bug.cgi?id=42
    * https://bugs.wine-staging.com/show_bug.cgi?id=149

--- dlls/dinput/mouse.c
+++ dlls/dinput/mouse.c
@@ -552,6 +552,8 @@ static HRESULT WINAPI SysMouseWImpl_GetDeviceState(LPDIRECTINPUTDEVICE8W iface, 
 
     if(This->base.acquired == 0) return DIERR_NOTACQUIRED;
 
+    MsgWaitForMultipleObjectsEx(0, NULL, 0, QS_ALLINPUT, 0);
+
     TRACE("(this=%p,0x%08x,%p):\n", This, len, ptr);
     _dump_mouse_state(&This->m_state);
 
