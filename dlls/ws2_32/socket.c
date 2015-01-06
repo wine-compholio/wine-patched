b5607ba88e1bb3b6758831bdcead0c53419f53ac
Heiko Przybyl <lil_tux@web.de>
2015-01-06 14:03:14 -0700

    ws2_32: Call SetLastError for successful WSARecv.

--- dlls/ws2_32/socket.c
+++ dlls/ws2_32/socket.c
@@ -6986,7 +6986,7 @@ static int WS2_recv_base( SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount,
     if (wsa != &localwsa) HeapFree( GetProcessHeap(), 0, wsa );
     release_sock_fd( s, fd );
     _enable_event(SOCKET2HANDLE(s), FD_READ, 0, 0);
-
+    WSASetLastError(0);
     return 0;
 
 error:
