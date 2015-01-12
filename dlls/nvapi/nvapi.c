15b8292106027556483d4a30d41df1418b941c16
Michael Müller <michael@fds-team.de>
2015-01-12 23:32:45 +0100

    nvapi: Explicity return NULL for 0x33c7358c and 0x593e8644.

--- dlls/nvapi/nvapi.c
+++ dlls/nvapi/nvapi.c
@@ -522,6 +522,8 @@ void* CDECL nvapi_QueryInterface(unsigned int offset)
         {0xaea3fa32, NvAPI_GetPhysicalGPUsFromLogicalGPU},
         {0xe5ac921f, NvAPI_EnumPhysicalGPUs},
         {0xceee8e9f, NvAPI_GPU_GetFullName},
+        {0x33c7358c, NULL}, /* This functions seems to be optional */
+        {0x593e8644, NULL}, /* This functions seems to be optional */
     };
     unsigned int i;
     TRACE("(%x)\n", offset);
