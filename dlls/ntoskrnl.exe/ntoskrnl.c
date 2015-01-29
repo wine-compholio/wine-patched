cb0aa2edd79d8d4f1a348d1ec4610cf25e145908
Alexander Morozov <amorozov@etersoft.ru>
2015-01-30 00:33:55 +0100

    ntoskrnl.exe: Improve KeInitializeTimerEx stub.

--- dlls/ntoskrnl.exe/ntoskrnl.c
+++ dlls/ntoskrnl.exe/ntoskrnl.c
@@ -1718,6 +1718,9 @@ void WINAPI KeInitializeSpinLock( PKSPIN_LOCK SpinLock )
 void WINAPI KeInitializeTimerEx( PKTIMER Timer, TIMER_TYPE Type )
 {
     FIXME( "stub: %p %d\n", Timer, Type );
+
+    RtlZeroMemory(Timer, sizeof(KTIMER));
+    Timer->Header.Type = Type ? 9 : 8;
 }
 
 
