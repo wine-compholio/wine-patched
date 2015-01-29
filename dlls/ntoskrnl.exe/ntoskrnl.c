d2721bfe40a080c0498b05ff1350b82513393fee
Alexander Morozov <amorozov@etersoft.ru>
2015-01-30 00:01:37 +0100

    ntoskrnl.exe: Add stub for ObReferenceObjectByPointer.

--- dlls/ntoskrnl.exe/ntoskrnl.c
+++ dlls/ntoskrnl.exe/ntoskrnl.c
@@ -2192,6 +2192,19 @@ NTSTATUS WINAPI ObReferenceObjectByName( UNICODE_STRING *ObjectName,
 
 
 /***********************************************************************
+ *           ObReferenceObjectByPointer   (NTOSKRNL.EXE.@)
+ */
+NTSTATUS WINAPI ObReferenceObjectByPointer(void *obj, ACCESS_MASK access,
+                                           POBJECT_TYPE type,
+                                           KPROCESSOR_MODE mode)
+{
+    FIXME("(%p, %x, %p, %d): stub\n", obj, access, type, mode);
+
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+
+/***********************************************************************
  *           ObDereferenceObject   (NTOSKRNL.EXE.@)
  */
 void WINAPI ObDereferenceObject( void *obj )
