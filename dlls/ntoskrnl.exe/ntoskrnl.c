cda08372006df98e10a85483de9771b5ea78388d
Christian Costa <titan.costa@gmail.com>
2015-02-11 09:43:20 +0100

    ntoskrnl.exe: Add stub for ExAcquireResourceExclusiveLite.

--- dlls/ntoskrnl.exe/ntoskrnl.c
+++ dlls/ntoskrnl.exe/ntoskrnl.c
@@ -1500,6 +1500,16 @@ NTSTATUS WINAPI ExInitializeResourceLite(PERESOURCE Resource)
 
 
 /***********************************************************************
+ *           ExAcquireResourceExclusiveLite   (NTOSKRNL.EXE.@)
+ */
+BOOLEAN WINAPI ExAcquireResourceExclusiveLite(PERESOURCE resource, BOOLEAN wait)
+{
+    FIXME("(%p, %u): stub\n", resource, wait);
+    return TRUE;
+}
+
+
+/***********************************************************************
  *           ExInitializeNPagedLookasideList   (NTOSKRNL.EXE.@)
  */
 void WINAPI ExInitializeNPagedLookasideList(PNPAGED_LOOKASIDE_LIST Lookaside,
