b1678d493716a016517cdf6adc61c2614d878dc2
Sebastian Lackner <sebastian@fds-team.de>
2015-02-27 01:29:54 +0100

    ntoskrnl.exe: Add stub for ProbeForWrite.

--- dlls/ntoskrnl.exe/ntoskrnl.c
+++ dlls/ntoskrnl.exe/ntoskrnl.c
@@ -2462,3 +2462,11 @@ VOID WINAPI ProbeForRead(PVOID address, SIZE_T length, ULONG alignment)
 {
     FIXME("%p %lu %u\n", address, length, alignment);
 }
+
+/***********************************************************************
+ *           ProbeForWrite   (NTOSKRNL.EXE.@)
+ */
+VOID WINAPI ProbeForWrite(PVOID address, SIZE_T length, ULONG alignment)
+{
+    FIXME("%p %lu %u\n", address, length, alignment);
+}
