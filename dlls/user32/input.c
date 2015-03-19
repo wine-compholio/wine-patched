e301f31744a82269fcd060c0cfa7c8ce72744fd0
Michael Müller <michael@fds-team.de>
2015-03-19 02:18:37 +0100

    user32: Get rid of wineserver call for GetLastInputInfo.

--- dlls/user32/input.c
+++ dlls/user32/input.c
@@ -487,6 +487,7 @@ done:
 BOOL WINAPI GetLastInputInfo(PLASTINPUTINFO plii)
 {
     BOOL ret;
+    shmglobal_t *shm = wine_get_shmglobal();
 
     TRACE("%p\n", plii);
 
@@ -494,6 +495,12 @@ BOOL WINAPI GetLastInputInfo(PLASTINPUTINFO plii)
     {
         SetLastError(ERROR_INVALID_PARAMETER);
         return FALSE;
+    }
+
+    if (shm)
+    {
+        plii->dwTime = shm->last_input_time;
+        return TRUE;
     }
 
     SERVER_START_REQ( get_last_input_time )
