wine-mirror/wine

commit 8826ba1bc86fefa22c543608585ae8f3b4310a5f

kernel32: Return proper char count in get_registry_locale_info.

dlls/kernel32/locale.c
@@ -1206,7 +1206,9 @@ static INT get_registry_locale_info( struct registry_value *registry_value, LPWS
         {
             if (status == STATUS_BUFFER_OVERFLOW && !buffer)
             {
-                ret = (size - info_size) / sizeof(WCHAR) + 1;
+                ret = (size - info_size) / sizeof(WCHAR);
+                if (!ret || ((WCHAR *)&info->Data)[ret-1])
+                    ret++;
             }
             else if (status == STATUS_OBJECT_NAME_NOT_FOUND)
             {

wine-compholio/wine-patched

commit cc219fc63bba7417f1e2270305772c66999adce9

kernel32: Fix calculation of returned buffer in get_registry_locale_info.
Fixes a regression introduced by 8826ba1bc86fefa22c543608585ae8f3b4310a5f.
We cannot decide if we have to append a terminating null char unless we have queried again with a bigger buffer.

dlls/kernel32/locale.c
@@ -1177,6 +1177,17 @@ static INT get_registry_locale_info( struct registry_value *registry_value, LPWS
 
         status = NtQueryValueKey( hkey, &nameW, KeyValuePartialInformation, info, size, &size );
 
+        /* try again with a bigger buffer when we have to return the correct size */
+        if (status == STATUS_BUFFER_OVERFLOW && !buffer && size > info_size)
+        {
+            KEY_VALUE_PARTIAL_INFORMATION *new_info;
+            if ((new_info = HeapReAlloc( GetProcessHeap(), 0, info, size )))
+            {
+                info = new_info;
+                status = NtQueryValueKey( hkey, &nameW, KeyValuePartialInformation, info, size, &size );
+            }
+        }
+
         NtClose( hkey );
 
         if (!status)
@@ -1207,8 +1218,6 @@ static INT get_registry_locale_info( struct registry_value *registry_value, LPWS
             if (status == STATUS_BUFFER_OVERFLOW && !buffer)
             {
                 ret = (size - info_size) / sizeof(WCHAR);
-                if (!ret || ((WCHAR *)&info->Data)[ret-1])
-                    ret++;
             }
             else if (status == STATUS_OBJECT_NAME_NOT_FOUND)
             {