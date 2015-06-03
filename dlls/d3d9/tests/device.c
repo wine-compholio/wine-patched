336da6ed8fbca0413e58ed6b25d788acb18970c4
Michael Müller <michael@fds-team.de>
2015-06-03 22:57:21 +0200

    winex11.drv: Allow changing the opengl pixel format on the desktop window.
    This patch is not 100% correct because the old behavior was more similar to
    windows. However, Direct3D supports using the desktop window to create a
    context and since Wine translates Direct3D to OpenGL, using the desktop
    window will fail because of this limitation. Unless someone comes up with
    a more correct solution or finds an application that breaks because of this,
    it seems to be a suitable workaround as it fixes multiple applications.

--- dlls/d3d9/tests/device.c
+++ dlls/d3d9/tests/device.c
@@ -11708,6 +11708,21 @@ static void test_destroyed_window(void)
     ok(!refcount, "Device has %u references left.\n", refcount);
 }
 
+static void test_desktop_window(void)
+{
+    IDirect3DDevice9 *device = NULL;
+    IDirect3D9 *d3d;
+
+    d3d = Direct3DCreate9(D3D_SDK_VERSION);
+    ok(!!d3d, "Failed to create a D3D object.\n");
+
+    device = create_device(d3d, GetDesktopWindow(), NULL);
+    ok(!!device, "Failed to created device on desktop window.\n");
+
+    if (device) IDirect3DDevice9_Release(device);
+    IDirect3D9_Release(d3d);
+}
+
 START_TEST(device)
 {
     WNDCLASSA wc = {0};
@@ -11827,6 +11842,7 @@ START_TEST(device)
     test_render_target_device_mismatch();
     test_format_unknown();
     test_destroyed_window();
+    test_desktop_window();
 
     UnregisterClassA("d3d9_test_wc", GetModuleHandleA(NULL));
 }
