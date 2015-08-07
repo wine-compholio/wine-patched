f0eea19477306f96fe49ee63e434ce924ea91d14
Michael Müller <michael@fds-team.de>
2015-08-07 03:44:39 +0200

    wininet: Allow INTERNET_OPTION_SETTINGS_CHANGED on connections.

--- dlls/wininet/internet.c
+++ dlls/wininet/internet.c
@@ -3045,6 +3045,9 @@ BOOL WINAPI InternetSetOptionW(HINTERNET hInternet, DWORD dwOption,
         ret = (res == ERROR_SUCCESS);
         break;
         }
+    case INTERNET_OPTION_SETTINGS_CHANGED:
+        FIXME("INTERNET_OPTION_SETTINGS_CHANGED; STUB\n");
+        break;
     default:
         FIXME("Option %d STUB\n",dwOption);
         SetLastError(ERROR_INTERNET_INVALID_OPTION);
