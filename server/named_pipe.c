d74c5acbe5649fd6cce5f40ad796257efe94d642
Sebastian Lackner <sebastian@fds-team.de>
2015-08-17 01:11:47 +0200

    server: Store a reference to the parent object for pipe servers.

--- server/named_pipe.c
+++ server/named_pipe.c
@@ -764,7 +764,7 @@ static struct pipe_server *create_pipe_server( struct named_pipe *pipe, unsigned
 {
     struct pipe_server *server;
 
-    server = alloc_object( &pipe_server_ops );
+    server = create_object( NULL, &pipe_server_ops, NULL, &pipe->obj );
     if (!server)
         return NULL;
 
@@ -786,11 +786,12 @@ static struct pipe_server *create_pipe_server( struct named_pipe *pipe, unsigned
     return server;
 }
 
-static struct pipe_client *create_pipe_client( unsigned int flags, unsigned int pipe_flags )
+static struct pipe_client *create_pipe_client( struct named_pipe *pipe, unsigned int flags,
+                                               unsigned int pipe_flags )
 {
     struct pipe_client *client;
 
-    client = alloc_object( &pipe_client_ops );
+    client = create_object( NULL, &pipe_client_ops, NULL, &pipe->obj );
     if (!client)
         return NULL;
 
@@ -876,7 +877,7 @@ static struct object *named_pipe_open_file( struct object *obj, unsigned int acc
         return NULL;
     }
 
-    if ((client = create_pipe_client( options, pipe->flags )))
+    if ((client = create_pipe_client( pipe, options, pipe->flags )))
     {
         type = ((pipe->flags & NAMED_PIPE_MESSAGE_STREAM_WRITE) && is_messagemode_supported()) ?
                SOCK_SEQPACKET : SOCK_STREAM;
