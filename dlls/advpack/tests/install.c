05490c0f27fa43f0c1d60b9b9eccc63a4af14dfb
Michael Müller <michael@fds-team.de>
2015-08-30 20:55:52 +0200

    advpack: Translate flags in LaunchINFSection.

--- dlls/advpack/tests/install.c
+++ dlls/advpack/tests/install.c
@@ -190,6 +190,8 @@ static void test_LaunchINFSection(void)
     HRESULT hr;
     char cmdline[MAX_PATH];
     static char file[] = "test.inf,DefaultInstall,4,0";
+    static char file2[] = "one\\test.inf,DefaultInstall,4,0";
+    static char file3[] = "test.inf,,1,0";
 
     /* The 'No UI' flag seems to have no effect whatsoever on Windows.
      * So only do this test in interactive mode.
@@ -207,8 +209,12 @@ static void test_LaunchINFSection(void)
     /* try a full path to the INF */
     lstrcpyA(cmdline, CURR_DIR);
     lstrcatA(cmdline, "\\");
-    lstrcatA(cmdline, "one\\test.inf,DefaultInstall,,4");
+    lstrcatA(cmdline, file2);
     hr = pLaunchINFSection(NULL, NULL, cmdline, 0);
+    ok(hr == 0, "Expected 0, got %d\n", hr);
+
+    /* try relative INF filename */
+    hr = pLaunchINFSection(NULL, NULL, file2, 0);
     ok(hr == 0, "Expected 0, got %d\n", hr);
 
     DeleteFileA("one\\test.inf");
@@ -218,6 +224,9 @@ static void test_LaunchINFSection(void)
 
     /* try just the INF filename */
     hr = pLaunchINFSection(NULL, NULL, file, 0);
+    ok(hr == 0, "Expected 0, got %d\n", hr);
+
+    hr = pLaunchINFSection(NULL, NULL, file3, 0);
     ok(hr == 0, "Expected 0, got %d\n", hr);
 
     DeleteFileA("test.inf");
