923a463edd5d6759b9579ebc68abab4f37f14749
Sebastian Lackner <sebastian@fds-team.de>
2015-11-05 01:55:50 +0100

    Revert "wined3d: Call wglGetPixelFormat() through the gl_ops table."
    This reverts commit b53256b8b752855576252cc2290dfbc9cb0f1280.

--- dlls/wined3d/context.c
+++ dlls/wined3d/context.c
@@ -947,7 +947,7 @@ static BOOL context_set_pixel_format(struct wined3d_context *context, HDC dc, BO
     if (dc == context->hdc && context->hdc_is_private && context->hdc_has_format)
         return TRUE;
 
-    current = gl_info->gl_ops.wgl.p_wglGetPixelFormat(dc);
+    current = GetPixelFormat(dc);
     if (current == format) goto success;
 
     if (!current)
@@ -1352,7 +1352,7 @@ static void context_enter(struct wined3d_context *context)
             context->needs_set = 1;
         }
         else if (!context->needs_set && !(context->hdc_is_private && context->hdc_has_format)
-                    && context->pixel_format != context->gl_info->gl_ops.wgl.p_wglGetPixelFormat(context->hdc))
+                    && context->pixel_format != GetPixelFormat(context->hdc))
             context->needs_set = 1;
     }
 }
@@ -1726,9 +1726,9 @@ struct wined3d_context *context_create(struct wined3d_swapchain *swapchain,
         goto out;
     }
 
-    ret->gl_info = gl_info;
-
     context_enter(ret);
+
+    ret->gl_info = gl_info;
 
     if (!context_set_pixel_format(ret, hdc, hdc_is_private, pixel_format))
     {
