9e90930ec41280c3f78a4b7272d98f648add33c5
Sebastian Lackner <sebastian@fds-team.de>
2015-11-15 04:25:27 +0100

    oleaut32: Initial preparation to make marshalling compatible with x86_64.

--- dlls/oleaut32/typelib.h
+++ dlls/oleaut32/typelib.h
@@ -618,7 +618,7 @@ static inline BOOL heap_free( LPVOID mem )
 
 HRESULT ITypeInfoImpl_GetInternalFuncDesc( ITypeInfo *iface, UINT index, const FUNCDESC **ppFuncDesc ) DECLSPEC_HIDDEN;
 
-extern DWORD _invoke(FARPROC func,CALLCONV callconv, int nrargs, DWORD *args) DECLSPEC_HIDDEN;
+extern DWORD _invoke(FARPROC func, CALLCONV callconv, int nrargs, DWORD_PTR *args) DECLSPEC_HIDDEN;
 
 HRESULT TMARSHAL_DllGetClassObject(REFCLSID rclsid, REFIID iid,LPVOID *ppv) DECLSPEC_HIDDEN;
 
