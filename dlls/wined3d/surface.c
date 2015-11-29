stefand/wine

wined3d: Put update_surface checks back in place
Unfortunately I can't remove the code from surface_update_from_surface
yet because blits depend on them.

dlls/wined3d/surface.c
@@ -1420,7 +1420,7 @@ static void surface_upload_data(struct wined3d_surface *surface, const struct wi
     }
 }
 
-static BOOL surface_check_block_align(struct wined3d_surface *surface, const RECT *rect)
+BOOL surface_check_block_align(struct wined3d_surface *surface, const RECT *rect)
 {
     struct wined3d_box box;
 

wine-compholio/wine-patched

dlls/wined3d/surface.c
@@ -1372,7 +1372,7 @@ static BOOL surface_check_block_align(struct wined3d_surface *surface, const str
     return wined3d_resource_check_block_align(&surface->resource, box);
 }
 
-static BOOL surface_check_block_align_rect(struct wined3d_surface *surface, const RECT *rect)
+BOOL surface_check_block_align_rect(struct wined3d_surface *surface, const RECT *rect)
 {
     struct wined3d_box box = {rect->left, rect->top, rect->right, rect->bottom, 0, 1};
 