e461092618e7b64b26d7d6d37bf546a2294dc965
Sebastian Lackner <sebastian@fds-team.de>
2015-12-04 10:36:47 +0100

    server: Introduce a new alloc_handle object callback. (v2)
    Signed-off-by: Sebastian Lackner <sebastian@fds-team.de>

--- server/async.c
+++ server/async.c
@@ -79,6 +79,7 @@ static const struct object_ops async_ops =
     no_link_name,              /* link_name */
     NULL,                      /* unlink_name */
     no_open_file,              /* open_file */
+    no_alloc_handle,           /* alloc_handle */
     no_close_handle,           /* close_handle */
     async_destroy              /* destroy */
 };
@@ -465,6 +466,7 @@ static const struct object_ops iosb_ops =
     no_link_name,             /* link_name */
     NULL,                     /* unlink_name */
     no_open_file,             /* open_file */
+    no_alloc_handle,          /* alloc_handle */
     no_close_handle,          /* close_handle */
     iosb_destroy              /* destroy */
 };
