b8b72d970e288dbda00f59b8dca11750d8c86545
Sebastian Lackner <sebastian@fds-team.de>
2015-12-04 10:36:47 +0100

    server: Introduce a new alloc_handle object callback. (v2)
    Signed-off-by: Sebastian Lackner <sebastian@fds-team.de>

--- server/console.c
+++ server/console.c
@@ -85,6 +85,7 @@ static const struct object_ops console_input_ops =
     default_set_sd,                   /* set_sd */
     no_lookup_name,                   /* lookup_name */
     no_open_file,                     /* open_file */
+    no_alloc_handle,                  /* alloc_handle */
     no_close_handle,                  /* close_handle */
     console_input_destroy             /* destroy */
 };
@@ -117,6 +118,7 @@ static const struct object_ops console_input_events_ops =
     default_set_sd,                   /* set_sd */
     no_lookup_name,                   /* lookup_name */
     no_open_file,                     /* open_file */
+    no_alloc_handle,                  /* alloc_handle */
     no_close_handle,                  /* close_handle */
     console_input_events_destroy      /* destroy */
 };
@@ -169,6 +171,7 @@ static const struct object_ops screen_buffer_ops =
     default_set_sd,                   /* set_sd */
     no_lookup_name,                   /* lookup_name */
     no_open_file,                     /* open_file */
+    no_alloc_handle,                  /* alloc_handle */
     no_close_handle,                  /* close_handle */
     screen_buffer_destroy             /* destroy */
 };
