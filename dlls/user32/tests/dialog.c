3f5570b2bdbe063c04ca4a8384a5a9f4c5fea162
Dmitry Timoshkov <dmitry@baikal.ru>
2016-02-18 10:22:29 +0800

    user32: MessageBox should be topmost when MB_SYSTEMMODAL style is set.

--- dlls/user32/tests/dialog.c
+++ dlls/user32/tests/dialog.c
@@ -1593,7 +1593,6 @@ static void test_MessageBox(void)
 
         hwnd = wait_for_window(params.caption);
         ex_style = GetWindowLongA(hwnd, GWL_EXSTYLE);
-        todo_wine_if(test[i].ex_style == WS_EX_TOPMOST)
         ok((ex_style & WS_EX_TOPMOST) == test[i].ex_style, "%d: got window ex_style %#x\n", i, ex_style);
 
         PostMessageA(hwnd, WM_COMMAND, IDCANCEL, 0);
@@ -1615,7 +1614,6 @@ static void test_MessageBox(void)
 
         hwnd = wait_for_window(params.caption);
         ex_style = GetWindowLongA(hwnd, GWL_EXSTYLE);
-        todo_wine_if(test[i].ex_style == WS_EX_TOPMOST)
         ok((ex_style & WS_EX_TOPMOST) == test[i].ex_style, "%d: got window ex_style %#x\n", i, ex_style);
 
         PostMessageA(hwnd, WM_COMMAND, IDCANCEL, 0);
