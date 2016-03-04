4467661c88a2a917dda05af2bbadc837d0a83758
Michael Müller <michael@fds-team.de>
2016-03-04 22:22:42 +0100

    ddraw: Set ddsOldCaps correctly in ddraw7_GetCaps.

--- dlls/ddraw/tests/ddraw2.c
+++ dlls/ddraw/tests/ddraw2.c
@@ -9604,6 +9604,31 @@ done:
     DestroyWindow(window);
 }
 
+static void test_caps(void)
+{
+    IDirectDraw2 *ddraw;
+    DDCAPS caps, caps2;
+    HRESULT hr;
+
+    ddraw = create_ddraw();
+    ok(!!ddraw, "Failed to create a ddraw object.\n");
+
+    caps.dwSize = sizeof(caps);
+    caps2.dwSize = sizeof(caps2);
+    hr = IDirectDraw2_GetCaps(ddraw, &caps, &caps2);
+    ok(SUCCEEDED(hr), "Failed to query for caps, hr %#x.\n", hr);
+
+    ok(caps.ddsOldCaps.dwCaps == caps.ddsCaps.dwCaps,
+       "Expected hal ddsOldCaps and ddsCaps to be identical (%x vs %x).\n",
+       caps.ddsOldCaps.dwCaps, caps.ddsCaps.dwCaps);
+
+    ok(caps2.ddsOldCaps.dwCaps == caps2.ddsCaps.dwCaps,
+       "Expected hel ddsOldCaps and ddsCaps to be identical (%x vs %x).\n",
+       caps2.ddsOldCaps.dwCaps, caps2.ddsCaps.dwCaps);
+
+    IDirectDraw2_Release(ddraw);
+}
+
 START_TEST(ddraw2)
 {
     IDirectDraw2 *ddraw;
@@ -9687,4 +9712,5 @@ START_TEST(ddraw2)
     test_yv12_overlay();
     test_offscreen_overlay();
     test_overlay_rect();
+    test_caps();
 }
