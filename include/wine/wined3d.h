199f105093f2912f22cb8aac82ba16df85589e7c
Michael Müller <michael@fds-team.de>
2016-08-23 22:47:56 +0200

    wined3d: Add 1d texture resource type.

--- include/wine/wined3d.h
+++ include/wine/wined3d.h
@@ -672,8 +672,9 @@ enum wined3d_resource_type
 {
     WINED3D_RTYPE_NONE                      = 0,
     WINED3D_RTYPE_BUFFER                    = 1,
-    WINED3D_RTYPE_TEXTURE_2D                = 2,
-    WINED3D_RTYPE_TEXTURE_3D                = 3,
+    WINED3D_RTYPE_TEXTURE_1D                = 2,
+    WINED3D_RTYPE_TEXTURE_2D                = 3,
+    WINED3D_RTYPE_TEXTURE_3D                = 4,
 };
 
 enum wined3d_pool
