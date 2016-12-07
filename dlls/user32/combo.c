5872c7cb18b89b46453e51d4983c1b92667139ff
Dmitry Timoshkov <dmitry@baikal.ru>
2016-12-07 16:10:31 +0800

    user32: Don't force a combobox repaint on WM_SIZE.
    This breaks z-order based painting and causes side effects for
    applications that during the WM_PAINT processing reference internal
    data associated with a not fully initialized window.

--- dlls/user32/combo.c
+++ dlls/user32/combo.c
@@ -1551,7 +1551,7 @@ static void COMBO_Size( LPHEADCOMBO lphc )
 		  &lphc->buttonRect,
 		  &lphc->droppedRect);
 
-  CBResetPos( lphc, &lphc->textRect, &lphc->droppedRect, TRUE );
+  CBResetPos( lphc, &lphc->textRect, &lphc->droppedRect, FALSE );
 }
 
 
