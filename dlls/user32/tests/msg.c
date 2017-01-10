a84af661616f7e7e7478f93dbbcd5e2d2abfe1b0
Dmitry Timoshkov <dmitry@baikal.ru>
2017-01-10 16:08:03 +0800

    user32: Add support for navigating a group of radio buttons using a keyboard.
    It approximates the behaviour observed in the message tests but still
    doesn't make the message tests pass without failures.

--- dlls/user32/tests/msg.c
+++ dlls/user32/tests/msg.c
@@ -6400,11 +6400,9 @@ if (0) /* actual message sequence is different on every run in some Windows setu
     while (PeekMessageA(&msg, 0, 0, 0, PM_REMOVE)) DispatchMessageA(&msg);
     ok_sequence(auto_radio_button_VK_DOWN_dialog, "IsDialogMessage(VK_DOWN)", TRUE);
 
-todo_wine
     test_radio(radio1, 1, radio2, 0, radio3, 0);
 
     hwnd = GetFocus();
-todo_wine
     ok(hwnd == radio1, "focus should be on radio1, not on %p\n", hwnd);
     GetWindowRect(radio1, &rc);
 
@@ -6419,11 +6417,9 @@ todo_wine
     while (PeekMessageA(&msg, 0, 0, 0, PM_REMOVE)) DispatchMessageA(&msg);
     ok_sequence(auto_radio_button_VK_DOWN_radio3, "down to radio3", TRUE);
 
-todo_wine
     test_radio(radio1, 1, radio2, 0, radio3, 0);
 
     hwnd = GetFocus();
-todo_wine
     ok(hwnd == radio1, "focus should be on radio1, not on %p\n", hwnd);
 
     flush_events();
@@ -6440,11 +6436,9 @@ todo_wine
     while (PeekMessageA(&msg, 0, 0, 0, PM_REMOVE)) DispatchMessageA(&msg);
     ok_sequence(auto_radio_button_VK_UP_radio1, "up to radio1", TRUE);
 
-todo_wine
     test_radio(radio1, 1, radio2, 0, radio3, 0);
 
     hwnd = GetFocus();
-todo_wine
     ok(hwnd == radio1, "focus should be on radio1, not on %p\n", hwnd);
 
     flush_events();
@@ -6462,7 +6456,6 @@ todo_wine
 if (0) /* actual message sequence is different on every run in some Windows setups */
     ok_sequence(auto_radio_button_VK_UP_dialog, "IsDialogMessage(VK_UP) #3", FALSE);
     /* what really matters is that nothing has changed */
-todo_wine
     test_radio(radio1, 1, radio2, 0, radio3, 0);
 
     log_all_parent_messages--;
