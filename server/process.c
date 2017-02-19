a6d5fb5ddaadb03f8fceee22e6c302381b02f496
Michael Müller <michael@fds-team.de>
2017-02-19 05:38:40 +0100

    kernel32: Make K32GetPerformanceInfo faster.

--- server/process.c
+++ server/process.c
@@ -1793,3 +1793,21 @@ DECL_HANDLER(set_job_completion_port)
 
     release_object( job );
 }
+
+/* Retrieve process, thread and handle count */
+DECL_HANDLER(get_system_info)
+{
+    struct process *process;
+
+    reply->processes = 0;
+    reply->threads = 0;
+    reply->handles = 0;
+
+    LIST_FOR_EACH_ENTRY( process, &process_list, struct process, entry )
+    {
+        if (!process->running_threads) continue;
+        reply->processes++;
+        reply->threads += process->running_threads;
+        reply->handles += get_handle_table_count( process );
+    }
+}
