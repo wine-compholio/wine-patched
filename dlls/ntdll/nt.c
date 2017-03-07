f02519109c2a46deae3a8cecf2fccef82a8d4169
Michael Müller <michael@fds-team.de>
2017-03-07 18:30:33 +0100

    ntdll: Implement SystemExtendedHandleInformation in NtQuerySystemInformation.

--- dlls/ntdll/nt.c
+++ dlls/ntdll/nt.c
@@ -2362,6 +2362,57 @@ NTSTATUS WINAPI NtQuerySystemInformation(
             RtlFreeHeap( GetProcessHeap(), 0, info );
         }
         break;
+    case SystemExtendedHandleInformation:
+        {
+            struct handle_info *info;
+            DWORD i, num_handles;
+
+            if (Length < sizeof(SYSTEM_HANDLE_INFORMATION_EX))
+            {
+                ret = STATUS_INFO_LENGTH_MISMATCH;
+                break;
+            }
+
+            if (!SystemInformation)
+            {
+                ret = STATUS_ACCESS_VIOLATION;
+                break;
+            }
+
+            num_handles = (Length - FIELD_OFFSET( SYSTEM_HANDLE_INFORMATION_EX, Handle ));
+            num_handles /= sizeof(SYSTEM_HANDLE_TABLE_ENTRY_INFO_EX);
+            if (!(info = RtlAllocateHeap( GetProcessHeap(), 0, sizeof(*info) * num_handles )))
+                return STATUS_NO_MEMORY;
+
+            SERVER_START_REQ( get_system_handles )
+            {
+                wine_server_set_reply( req, info, sizeof(*info) * num_handles );
+                if (!(ret = wine_server_call( req )))
+                {
+                    SYSTEM_HANDLE_INFORMATION_EX *shi = SystemInformation;
+                    shi->Count = wine_server_reply_size( req ) / sizeof(*info);
+                    shi->Reserved = 0;
+                    len = FIELD_OFFSET( SYSTEM_HANDLE_INFORMATION_EX, Handle[shi->Count] );
+                    for (i = 0; i < shi->Count; i++)
+                    {
+                        memset( &shi->Handle[i], 0, sizeof(shi->Handle[i]) );
+                        shi->Handle[i].UniqueProcessId = info[i].owner;
+                        shi->Handle[i].HandleValue     = info[i].handle;
+                        shi->Handle[i].GrantedAccess   = info[i].access;
+                        /* FIXME: Fill out remaining fields */
+                    }
+                }
+                else if (ret == STATUS_BUFFER_TOO_SMALL)
+                {
+                    len = FIELD_OFFSET( SYSTEM_HANDLE_INFORMATION_EX, Handle[reply->count] );
+                    ret = STATUS_INFO_LENGTH_MISMATCH;
+                }
+            }
+            SERVER_END_REQ;
+
+            RtlFreeHeap( GetProcessHeap(), 0, info );
+        }
+        break;
     case SystemCacheInformation:
         {
             SYSTEM_CACHE_INFORMATION sci;
