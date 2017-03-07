937c66f655da6489a4551f8d8c14255529ecc4a5
Michael Müller <michael@fds-team.de>
2017-03-07 01:57:42 +0100

    ntdll: Implement ProcessImageFileNameWin32 in NtQueryInformationProcess.

--- dlls/ntdll/process.c
+++ dlls/ntdll/process.c
@@ -536,6 +536,7 @@ NTSTATUS WINAPI NtQueryInformationProcess(
     case ProcessImageFileName:
         /* FIXME: this will return a DOS path. Windows returns an NT path. Changing this would require also changing kernel32.QueryFullProcessImageName.
          * The latter may be harder because of the lack of RtlNtPathNameToDosPathName. */
+    case ProcessImageFileNameWin32:
         SERVER_START_REQ(get_dll_info)
         {
             UNICODE_STRING *image_file_name_str = ProcessInformation;
