deda32ce6fed8ca96940a3081dbd5285007f2501
Michael Müller <michael@fds-team.de>
2017-03-08 03:44:19 +0100

    server: Rename ObjectType to Type.

--- server/object.h
+++ server/object.h
@@ -251,11 +251,11 @@ static const WCHAR type_Job[] =           {'J','o','b'};
 static const WCHAR type_Key[] =           {'K','e','y'};
 static const WCHAR type_KeyedEvent[] =    {'K','e','y','e','d','E','v','e','n','t'};
 static const WCHAR type_Mutant[] =        {'M','u','t','a','n','t'};
-static const WCHAR type_ObjectType[] =    {'O','b','j','e','c','t','T','y','p','e'};
 static const WCHAR type_Section[] =       {'S','e','c','t','i','o','n'};
 static const WCHAR type_Semaphore[] =     {'S','e','m','a','p','h','o','r','e'};
 static const WCHAR type_SymbolicLink[] =  {'S','y','m','b','o','l','i','c','L','i','n','k'};
 static const WCHAR type_Timer[] =         {'T','i','m','e','r'};
+static const WCHAR type_Type[] =          {'T','y','p','e'};
 static const WCHAR type_WindowStation[] = {'W','i','n','d','o','w','S','t','a','t','i','o','n'};
 
 extern void init_types(void);
