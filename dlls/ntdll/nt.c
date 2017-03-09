2c93769a1dc8296fdd45a5a1ed21e3d04a572688
Michael Müller <michael@fds-team.de>
2017-03-09 16:27:23 +0100

    ntdll: Fill process kernel and user time.

--- dlls/ntdll/nt.c
+++ dlls/ntdll/nt.c
@@ -1922,6 +1922,7 @@ NTSTATUS WINAPI NtQuerySystemInformation(
         {
             SYSTEM_PROCESS_INFORMATION* spi = SystemInformation;
             SYSTEM_PROCESS_INFORMATION* last = NULL;
+            unsigned long clk_tck = sysconf(_SC_CLK_TCK);
             HANDLE hSnap = 0;
             WCHAR procname[1024];
             WCHAR* exename;
@@ -1959,7 +1960,7 @@ NTSTATUS WINAPI NtQuerySystemInformation(
 
                         if (Length >= len + procstructlen)
                         {
-                            /* ftCreationTime, ftUserTime, ftKernelTime;
+                            /* ftCreationTime;
                              * vmCounters, ioCounters
                              */
  
@@ -1977,6 +1978,9 @@ NTSTATUS WINAPI NtQuerySystemInformation(
 
                             /* spi->ti will be set later on */
 
+                            if (reply->unix_pid != -1)
+                                read_process_time(reply->unix_pid, -1, clk_tck,
+                                                  &spi->KernelTime, &spi->UserTime);
                         }
                         len += procstructlen;
                     }
