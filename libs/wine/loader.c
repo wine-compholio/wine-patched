5e5a6e18539afb2015332507785d1e75fad780fa
Michael Müller <michael@fds-team.de>
2017-05-25 21:56:06 +0200

    libs/wine: Use same file alignment for fake and builtin DLLs.

--- libs/wine/loader.c
+++ libs/wine/loader.c
@@ -455,7 +455,7 @@ static void *map_dll( const IMAGE_NT_HEADERS *nt_descr )
     sec->SizeOfRawData = data_start - code_start;
     sec->Misc.VirtualSize = sec->SizeOfRawData;
     sec->VirtualAddress   = code_start;
-    sec->PointerToRawData = code_start;
+    sec->PointerToRawData = 0x200; /* file alignment */
     sec->Characteristics  = (IMAGE_SCN_CNT_CODE | IMAGE_SCN_MEM_EXECUTE | IMAGE_SCN_MEM_READ);
     sec++;
 
