0a961fc0e18fa00fcebc078c9c2d8a38d719f237
Sebastian Lackner <sebastian@fds-team.de>
2017-05-28 11:17:26 +0200

    ntdll: Resolve drive symlinks before returning section name.

--- dlls/ntdll/ntdll_misc.h
+++ dlls/ntdll/ntdll_misc.h
@@ -167,6 +167,7 @@ extern NTSTATUS nt_to_unix_file_name_attr( const OBJECT_ATTRIBUTES *attr, ANSI_S
                                            UINT disposition ) DECLSPEC_HIDDEN;
 
 /* virtual memory */
+extern NTSTATUS read_nt_symlink( HANDLE root, UNICODE_STRING *name, WCHAR *target, size_t length ) DECLSPEC_HIDDEN;
 extern void virtual_get_system_info( SYSTEM_BASIC_INFORMATION *info ) DECLSPEC_HIDDEN;
 extern NTSTATUS virtual_create_builtin_view( void *base ) DECLSPEC_HIDDEN;
 extern NTSTATUS virtual_alloc_thread_stack( TEB *teb, SIZE_T reserve_size, SIZE_T commit_size ) DECLSPEC_HIDDEN;
