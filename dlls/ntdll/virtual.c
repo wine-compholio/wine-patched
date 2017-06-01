8ca7f73af199bb2ad57590ba03eb5fe1bd51a3f7
Michael Müller <michael@fds-team.de>
2017-06-01 06:04:53 +0200

    ntdll: Fix holes in ELF mappings. (v2)
    Based on a patch by Andrew Wesie.

--- dlls/ntdll/virtual.c
+++ dlls/ntdll/virtual.c
@@ -1794,6 +1794,20 @@ NTSTATUS virtual_handle_fault( LPCVOID addr, DWORD err, BOOL on_signal_stack )
             if (VIRTUAL_GetUnixProt( get_page_vprot( page )) & PROT_READ) ret = STATUS_SUCCESS;
             else update_shared_data = FALSE;
         }
+        else if (!err && (view->protect & VPROT_SYSTEM) && (VIRTUAL_GetUnixProt( vprot ) & PROT_READ))
+        {
+            int unix_prot = VIRTUAL_GetUnixProt( vprot );
+            unsigned char vec;
+
+            if (!VIRTUAL_SetProt( view, page, page_size, vprot ))
+                set_page_vprot_bits( page, page_size, 0, VPROT_READ | VPROT_EXEC );
+            else if (!mincore( page, page_size, &vec ) && (vec & 1))
+                ret = STATUS_SUCCESS;
+            else if (wine_anon_mmap( page, page_size, unix_prot, MAP_FIXED ) != page)
+                set_page_vprot_bits( page, page_size, 0, VPROT_READ | VPROT_EXEC );
+            else
+                ret = STATUS_SUCCESS;
+        }
         if (!on_signal_stack && (vprot & VPROT_GUARD))
         {
             set_page_vprot_bits( page, page_size, 0, VPROT_GUARD );
