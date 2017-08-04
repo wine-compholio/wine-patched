d7437bd0b8e6848c8e5ba07ecf4901c3026b5715
Michael Müller <michael@fds-team.de>
2017-08-05 01:45:29 +0200

    ntdll: Add function to create new tokens for elevation purposes.

--- dlls/ntdll/process.c
+++ dlls/ntdll/process.c
@@ -99,6 +99,24 @@ HANDLE CDECL __wine_make_process_system(void)
     return ret;
 }
 
+/***********************************************************************
+ *           __wine_create_default_token   (NTDLL.@)
+ *
+ * Creates a default limited or admin token.
+ */
+HANDLE CDECL __wine_create_default_token( BOOL admin )
+{
+    HANDLE ret = NULL;
+    SERVER_START_REQ( create_token )
+    {
+        req->admin = admin;
+        if (!wine_server_call( req ))
+            ret = wine_server_ptr_handle( reply->token );
+    }
+    SERVER_END_REQ;
+    return ret;
+}
+
 static UINT process_error_mode;
 
 #define UNIMPLEMENTED_INFO_CLASS(c) \
