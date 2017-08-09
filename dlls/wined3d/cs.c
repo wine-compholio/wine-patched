0556964506c3cd876f6bfd17483c49da3cc3ab7a
Sebastian Lackner <sebastian@fds-team.de>
2017-08-09 06:32:43 +0200

    wined3d: Remaining UAV counter changes.
    Based on patches by Michael Müller and Józef Kucia.

--- dlls/wined3d/cs.c
+++ dlls/wined3d/cs.c
@@ -2293,6 +2293,7 @@ static void wined3d_cs_exec_copy_uav_counter(struct wined3d_cs *cs, const void *
     context_release(context);
 
     wined3d_resource_release(&op->buffer->resource);
+    wined3d_resource_release(view->resource);
 }
 
 void wined3d_cs_emit_copy_uav_counter(struct wined3d_cs *cs, struct wined3d_buffer *dst_buffer,
@@ -2307,6 +2308,7 @@ void wined3d_cs_emit_copy_uav_counter(struct wined3d_cs *cs, struct wined3d_buff
     op->view = uav;
 
     wined3d_resource_acquire(&dst_buffer->resource);
+    wined3d_resource_acquire(uav->resource);
 
     cs->ops->submit(cs, WINED3D_CS_QUEUE_DEFAULT);
 }
